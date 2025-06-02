d5b3a078c74931affac05cdfe8738e48b86276da
Ravel <ravel.medeiro@gmail.com>
2025-06-01 21:00:45 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,11 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)", // aplica para todas as rotas
+        source: "/(.*)",
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-inline' https://analytics.tiktok.com;",
+            value: "script-src 'self' https://analytics.tiktok.com 'sha256-W+y2eHzc9i5el1db9QMo9xqkKryyTN7S51duRkzq5Rg=';",
           },
         ],
       },
