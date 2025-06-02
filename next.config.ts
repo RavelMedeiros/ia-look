db2df077277eac5f2807d284784de59177e07aac
Ravel <ravel.medeiro@gmail.com>
2025-06-01 20:56:40 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // aplica para todas as rotas
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' https://analytics.tiktok.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
